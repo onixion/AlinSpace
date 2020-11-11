06b9e5ab05880d113a9b999f9cc17210dff90dc0
EC2 Default User <ec2-user@ip-172-31-44-157.eu-west-1.compute.internal>
2020-11-11 19:17:46 +0000

    Updated script

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo yum install docker
+sudo service docker enable
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
 sudo chmod +xxx /bin/docker-compose
